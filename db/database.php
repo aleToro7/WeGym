ec780cf86db3db96256a09ca49d93a37f812138f
Motta <roberto.dellamotta2@studio.unibo.it>
2023-12-31 16:05:57 +0100

    login non criptato

--- db/database.php
+++ db/database.php
@@ -13,11 +13,12 @@ class DatabaseHelper{
     //metodi per query
 
     public function checkLogin($username, $enc_password){
-        $salt = "asd832jiaiodsjioa";
-        $salted_pwd = $salt + $enc_password;
+        //$salt = "asd832jiaiodsjioa";
+        //$salted_pwd = $salt + $enc_password;
         $query = "SELECT nomeUtente, nome, cognome FROM utente WHERE nomeUtente = ? AND password = ?";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('ss',$username, $salted_pwd);
+        //$stmt->bind_param('ss',$username, $salted_pwd);
+        $stmt->bind_param('ss',$username, $enc_password);
         $stmt->execute();
         $result = $stmt->get_result();
 
--- index.php
+++ index.php
@@ -6,9 +6,12 @@ if(isset($_POST['Login'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        $enc_password = password_hash($password, PASSWORD_DEFAULT);
+        //$enc_password = password_hash($password, PASSWORD_DEFAULT);
 
-        $login_result = $dbh->checkLogin($username, $enc_password);
+        $login_result = $dbh->checkLogin($username, $password);
+        //$login_result = $dbh->checkLogin($username, $enc_password);
+
+
         if(count($login_result)==0){
             //Login fallito
             $templateParams["errorelogin"] = "Errore! Controllare username o password!";
@@ -20,7 +23,7 @@ if(isset($_POST['Login'])){
 }
 
 if(isUserLoggedIn()){
-    //TO DO
+    header("location: /home.php");
 }else{
     //TO DO
 }
