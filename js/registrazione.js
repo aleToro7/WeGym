4cf55f0ca0b7b017db1cbd54d02753b72c2aa73b
AleToro7 <toryst4rk@gmail.com>
2023-12-31 11:52:18 +0100

    func manageBtnContinua() in registrazione

--- js/registrazione.js
+++ js/registrazione.js
@@ -10,24 +10,49 @@ $(document).ready(function () {
 
     });*/
 
+    $("#nome").keyup(function() {
+        manageBtnContinua();
+    });
+
+    $("#cognome").keyup(function() {
+        manageBtnContinua();
+    });
+
     $("#username").keyup(function() {
         username = $("#username").val();
-
-        jQuery.ajax({
-            type:'POST',
-            url:'../registrazione.php',
-            data: 'username=' + username,
-            success: function(data) {
-                $("#user-availability-status").html(data);
-                if(data == "Username available"){
-                    $("#continua").removeAttr("disabled");
-                }else if(data == "Username not available" || $("#username").val() == ""){
-                    $("#continua").attr('disabled','disabled');
-                }
-            },
-            error: function() { }
-        });
+        if(username.length > 3){
+            jQuery.ajax({
+                type:'POST',
+                url:'../registrazione.php',
+                data: 'username=' + username,
+                success: function(data) {
+                    $("#user-availability-status").html(data);
+                    if(data == "Username available"){
+                        $("#username").removeClass("border border-3 border-danger");
+                        $("#username").addClass("border border-3 border-success");
+                    }else if(data == "Username not available" || $("#username").val() == ""){
+                        $("#username").removeClass("border border-3 border-success");
+                        $("#username").addClass("border border-3 border-danger");
+                    }
+                    manageBtnContinua();
+                },
+                error: function() { }
+            });
+        }else{
+            $("#user-availability-status").html("Il nome utente deve avere un numero di caratteri compreso tra 4 e 25");
+            $("#username").removeClass("border border-3 border-success");
+            $("#username").addClass("border border-3 border-danger");
+        }
+        
     });
+
+    function manageBtnContinua(){
+        if($("#username").hasClass("border-success") && $("#nome").val() != "" && $("#cognome").val() != ""){
+            $("#continua").removeAttr("disabled");
+        }else{
+            $("#continua").attr('disabled','disabled');
+        }
+    }
 
 });
 
--- registrazione.php
+++ registrazione.php
@@ -5,7 +5,7 @@ $username = "";
 if(isset($_POST['username']) || !empty($_POST['username']))
 {
    $username = strip_tags(trim($_POST['username']));
-   if(strlen($username) < 4 && strlen($username) < 25){
+   if(strlen($username) < 4 && strlen($username) < 25){  //strlen($username) < 4 potenzialmente inutile dato che non viene effettuato il post finche non viene inserito il 4 carattere
       echo "Il nome utente deve avere un numero di caratteri compreso tra 4 e 25";
    }else{
       $already_taken = $dbh->checkUserName($username);
