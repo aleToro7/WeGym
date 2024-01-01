8fec6f57408a7732375a35d64cab1b3a7fb28873
Motta <roberto.dellamotta2@studio.unibo.it>
2024-01-01 18:54:01 +0100

    navbar

--- template/base.php
+++ template/base.php
@@ -10,11 +10,25 @@
 
     <title>Home</title>
 </head>
-<body> 
+<body class="bgbody">
     <div>
         
     </div>
 
+
+    <div>
+        <nav class=" d-flex bd-highlight fixed-bottom">
+            <a class="nav p-2 flex-fill bd-highlight " href="#">
+                <i class="bi bi-house-door-fill fa-2x" id="home"></i>
+            </a>
+            <a class="nav p-2 flex-fill bd-highlight" href="#">
+                <i class="bi bi-plus-square fa-2x" id="upload"></i>
+            </a>
+            <a class="nav p-2 flex-fill bd-highlight" href="#">
+                <i class="bi bi-person fa-2x" id="profile"></i>
+            </a>
+        </nav>
+    </div>
 </body>
 </html>
 
