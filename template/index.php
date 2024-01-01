7514b597bbbea6541b5a17a903ef1e3a00f7af67
Motta <roberto.dellamotta2@studio.unibo.it>
2024-01-01 13:42:54 +0100

    login

--- template/index.php
+++ template/index.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="../js/login.js"></script>
 </head>
 <body class="bgbody">
 <div class="my-container">
@@ -22,8 +24,9 @@
                 <li>
                     <input type="password" id="password" name="password" placeholder="Password:"/>
                 </li>
+                <div id="login-error"></div>
                 <li>
-                    <button class="btn-login" type="submit" name="Login">Login</button>
+                    <button class="btn-login" type="button" id="login">Login</button>
                 </li>
             </ul>
             <div>
