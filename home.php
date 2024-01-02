8b93f17de7a3cdf4d931e98cce13ae7dd5047098
AleToro7 <toryst4rk@gmail.com>
2024-01-03 00:58:54 +0100

    aggiustato home.php

--- home.php
+++ home.php
@@ -1,19 +1,13 @@
 <?php
-
 require_once 'bootstrap.php';
 
-    if(!isset($_SESSION["username"])){
-        header("location: index.php");
-        exit;
+if(isset($_POST["cercaFromAjax"])) {
+    $userCercato = strip_tags(trim($_POST['cercaFromAjax']));
+    $results = $dbh->searchUser($userCercato);
+    if (count($results) > 0) {
+        echo json_encode($results);
+    }else {
+        echo "Nessun utente trovato";
     }
-
-    if(isset($_POST["cercaFromAjax"])) {
-        $userCercato = strip_tags(trim($_POST['cercaFromAjax']));
-        $results = $dbh->searchUser($userCercato);
-        if (count($results) > 0) {
-            echo json_encode($results);
-        }else {
-            echo "Nessun utente trovato";
-        }
-    }
+}
 ?>
--- template/base.php
+++ template/base.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<?php
+    session_start();
+    if(!isset($_SESSION["username"])){
+        header("location: index.php");
+        exit;
+    }
+?>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
