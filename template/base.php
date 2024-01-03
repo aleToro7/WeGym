89c44dbd29b7a5817887e3d464bb6da75bf20ba7
AleToro7 <toryst4rk@gmail.com>
2024-01-03 20:44:05 +0100

    inizio di crop

--- template/base.php
+++ template/base.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css"  /><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
     <title>WeGym</title>
     <link rel="icon" type="image/x-icon" href="../altro/favicon.png">
 </head>
--- template/modifica-profilo.php
+++ template/modifica-profilo.php
@@ -1,8 +1,46 @@
 <script src="../js/modifica-profilo.js"></script>
+<script src="../js/crop.js"></script>
 <div class="container-fluid">
     <div class="row">
         <div class="col">
             <i class="close bi bi-x" id="close"></i>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="profile-img-container"></div>
+            <form method="post">
+                <input type="file" name="image button" class="image" value="Scegli immagine profilo">
+            </form>
+        </div>
+        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalLabel">Crop image</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="img-container">
+                            <div class="row">
+                                <div class="col-md-8">  
+                                    <!--  default image where we will set the src via jquery-->
+                                    <img id="image" class="img-crop">
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="preview profile-img-container"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary" id="crop">Crop</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
