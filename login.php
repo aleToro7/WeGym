e8dde50fcbed5ee052a1c8e8ba8ace5924992e5d
AleToro7 <toryst4rk@gmail.com>
2024-01-04 19:32:14 +0100

    fix login

--- login.php
+++ login.php
@@ -7,11 +7,12 @@ if(isset($_POST['username']) && isset($_POST['password'])){
     $password = strip_tags(trim($_POST['password']));
 
     $login_result = $dbh->checkLogin($username);
-    $hash = $login_result[0]['password'];
+    
     if(count($login_result)==0){
         //Login fallito
         echo "Errore! Username non corretto.";
     }else {
+        $hash = $login_result[0]['password'];
         if(password_verify($password, $hash)) {
             registerLoggedUser($login_result[0]);
             echo "ok";
