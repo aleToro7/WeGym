eb933cbfc8a230ebf7d3942cab43adda273cc4e0
Motta <roberto.dellamotta2@studio.unibo.it>
2024-01-15 19:27:43 +0100

    upload post

--- template/carica-post.php
+++ template/carica-post.php
@@ -1,5 +1,36 @@
-<div class="upload-post ">
-    <p>ciao</p>
+<?php
+    require_once '../profili.php';
+?>
+
+<div class="upload-post">
+    <div class="container-fluid">
+        <br>
+        <div class="row">
+            <div class="col" name="profile-img">
+                <img class="profile-img-container-upload" id="img-profile-upload-post" src="<?php 
+                    if($templateParams["imgProfilo"]!='') {
+                        echo $templateParams["imgProfilo"];
+                    }else {
+                        echo "../altro/img_avatar.png";
+                    }
+                ?>"/>
+                <span class="usrname" id="usrname"><?php echo $templateParams["username"];?></span><br>
+            </div>
+        </div>
+        <div class="row">
+            
+        </div>
+        <div class="row">
+            <div name="text-post">
+                <textarea class="post-text" rows="8" placeholder="Scrivi.." required></textarea>
+            </div>
+        </div>
+    </div>
+    <div class="green-upload"></div>    
+    <div>
+        <i class="upload-icon bi bi-image"></i>
+        <button type="button" class="button button-post" id="posta">Post <i class="bi bi-arrow-right"></i></button> 
+    </div>
 </div>
 <?php
 
