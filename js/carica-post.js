afb43becbfc6b606de16dde5a138152221aa2241
AleToro7 <toryst4rk@gmail.com>
2024-01-15 20:02:21 +0100

    qwdasd

--- js/carica-post.js
+++ js/carica-post.js
@@ -1,3 +1,84 @@
 function eventiCaricaPost(){
+    waitForEl("#posta", function() {
+        $("#close").click(function(){
+            $("#load").empty();
+            $("#load").load('./carica-post.php', eventiProfilo());
+        });
+    
+        $("#close-crop").click(function(){
+            bs_modal.modal('hide');
+            $("#getImage").val('');
+        });
+    
+        let bs_modal = $('#modal');
+        let image = document.getElementById('image');
+        let cropper,reader,file;
+    
+        $("#getPostImage").on("change", function(e) {
+            let files = e.target.files;
+            let done = function(url) {
+                image.src = url;
+                bs_modal.modal('show');
+            };
+    
+    
+            if (files && files.length > 0) {
+                file = files[0];
+    
+                if (URL) {
+                    done(URL.createObjectURL(file));
+                } else if (FileReader) {
+                    reader = new FileReader();
+                    reader.onload = function(e) {
+                        done(reader.result);
+                    };
+                    reader.readAsDataURL(file);
+                }
+            }
+        });
+    
+        bs_modal.on('shown.bs.modal', function() {
+            cropper = new Cropper(image, {
+                aspectRatio: 1,
+                viewMode: 3,
+                preview: '.preview'
+            });
+        }).on('hidden.bs.modal', function() {
+            cropper.destroy();
+            cropper = null;
+            bs_modal.modal('hide');
+            $("#getImage").val('');
+        });
+    
+        $("#crop").click(function() {
+            canvas = cropper.getCroppedCanvas({
+                width: 160,
+                height: 160,
+            });
+    
+            canvas.toBlob(function(blob) {
+                url = URL.createObjectURL(blob);
+                let reader = new FileReader();
+                reader.readAsDataURL(blob);
+                
+                reader.onloadend = function() {
+                    let base64data = reader.result;
+                    $.ajax({
+                        type: "POST",
+                        url: "../modificaProfilo.php",
+                        data: {imageFromAjax: base64data},
+                        success: function(data) {
+                            if(data == "ok") {
+                                bs_modal.modal('hide');
+                                $("#getImage").val('');
+                                $("#img-profile").attr('src', base64data);
+                                $(".preview-img-container").attr('src', base64data); 
+                            }
+                        }
+                    });
+                }
+            });
+        });
+    });
     
 }
--- template/carica-post.php
+++ template/carica-post.php
@@ -18,7 +18,7 @@
             </div>
         </div>
         <div class="row">
-            
+            <div class="hidden" id="postImage"></div>
         </div>
         <div class="row">
             <div name="text-post">
@@ -28,8 +28,35 @@
     </div>
     <div class="green-upload"></div>    
     <div>
-        <i class="upload-icon bi bi-image"></i>
+        <input type="file" class="upload-icon bi bi-image" id="getPostImage">
+        
         <button type="button" class="button button-post" id="posta" disabled>Post <i class="bi bi-arrow-right"></i></button> 
+    </div>
+</div>
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content" style="background-color: #373737">
+            <div class="modal-header">
+                <h5 class="modal-title nome-utente" id="modalLabel">Ritaglia immagine</h5>
+                <i class="close bi bi-x" id="close-crop"></i>
+            </div>
+            <div class="modal-body">
+                <div class="img-container">
+                    <div class="row">
+                        <div class="col-md-8"> 
+                            <img id="image" class="img-crop">
+                        </div>
+                        <div class="col-md-4">
+                            <div class="preview post-img-container"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button btn-primary" id="crop">Ritaglia</button>
+            </div>
+            <span><?php if(isset($templateParams['erroreImmagine'])) echo $templateParams['erroreImmagine'];?></span>
+        </div>
     </div>
 </div>
 <?php
