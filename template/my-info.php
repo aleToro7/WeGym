baa0489358db8940b88c654aa9e346becdf33118
Motta <roberto.dellamotta2@studio.unibo.it>
2024-01-18 19:21:34 +0100

    myinfo

--- template/my-info.php
+++ template/my-info.php
@@ -4,7 +4,29 @@
         foreach($templateParams["myinfo"] as $myinfo){
         ?>
             <div class="info-container">
-                <p>ciao</p>
+                <img class="my-info-profilo" src="../altro/myinfo-gray.png" alt="MyInfo">
+                <div class="row">
+                    <div>
+                        <ul class="less-points">
+                            <li>
+                                <h6>Quanti giorni alla settimana mi alleno: </h6>
+                                <span><?php echo $myinfo["frequenzaAllenamenti"]; ?></span>
+                            </li>
+                            <li>
+                                <span>a</span>
+                                <span><?php echo $myinfo["obbiettivo"]; ?></span>
+                            </li>
+                            <li>
+                                <span></span>
+                                <span><?php echo $myinfo["esercizioPreferito"]; ?></span>
+                            </li>
+                            <li>
+                                <span></span>
+                                <span></span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         <?php
         }
